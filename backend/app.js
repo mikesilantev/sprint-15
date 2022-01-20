c87062c13a5ec0d20824761d9f6114e3d7585245
Mike <77847034+mikesilantev@users.noreply.github.com>
2022-01-20 15:37:35 +0300

    FIX: signup Joi pattern and add new RegExp

--- backend/app.js
+++ backend/app.js
@@ -75,7 +75,7 @@ app.post(
       email: Joi.string().required().email(),
       password: Joi.string()
         .required()
-        .regex(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\S+$).{8,}$/),
+        .pattern(new RegExp(/^[A-Za-z0-9]{8,30}$/)),
       name: Joi.string().regex(/^[a-z0-9_-]{2,30}$/),
       about: Joi.string().min(2).max(30),
       avatar: Joi.string().regex(
