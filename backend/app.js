660cd08416cbf5b6fc0538b5a7e5ab34ca266431
Mike <77847034+mikesilantev@users.noreply.github.com>
2022-01-21 15:06:33 +0300

    BACK: dotenv v1

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
@@ -28,8 +29,6 @@ const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
 });
-
-require('dotenv').config();
 
 app.use(limiter);
 app.use(helmet());
