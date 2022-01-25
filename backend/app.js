9be3e4c1f52dbdf5984f7ec61d9118747e1fde4f
Mike <77847034+mikesilantev@users.noreply.github.com>
2022-01-25 18:02:53 +0300

    CORS

--- backend/app.js
+++ backend/app.js
@@ -30,6 +30,30 @@ const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
 });
+const allowedCors = [
+  'https://mike.nomoredomains.rocks',
+  'http://mike.nomoredomains.rocks',
+  'http://localhost:3000'
+];
+
+app.use(function(req, res, next) {
+  const { origin } = req.headers;
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if(allowedCors.includes(origin)){
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+  }
+
+  if(method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  next();
+});
 
 app.use(requestLogger);
 app.use(limiter);
