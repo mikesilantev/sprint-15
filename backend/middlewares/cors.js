7d17f4421439ce1cf1c806d1868401c0eb291b16
Mike <77847034+mikesilantev@users.noreply.github.com>
2022-01-28 17:42:35 +0300

    FIX: BACK cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -2,6 +2,8 @@ const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 const allowedCors = [
   'http://localhost:3000',
   'https://localhost:3000',
+  'http://mike.nomoredomains.rocks',
+  'https://mike.nomoredomains.rocks',
 ];
 
 module.exports = (req, res, next) => {
